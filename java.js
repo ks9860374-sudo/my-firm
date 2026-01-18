8a48f0b48c7b4bbbbbd38af283c6807289ab85d2
ks9860374-sudo <ks9860374@gmail.com>
2026-01-18 18:04:20 +0530

    Add files via upload
    update

---
+++ java.js
@@ -0,0 +1,10 @@
+document.getElementById('contactBtn').addEventListener('click', () => {
+    alert('Thank you for reaching out! We will contact you shortly.');
+    document.getElementById('homeLink').addEventListener('click', (e) => {
+        // Optional: Prevent the page from jumping if you just want the image
+        // e.preventDefault(); 
+    
+        const container = document.getElementById('imageContainer');
+        
+    });
+});
